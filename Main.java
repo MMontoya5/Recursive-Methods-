637906de4f4cd92966b7c515d35418324ba0fecc
MMontoya5 <>
2021-04-15 22:18:44 +0000

    Actually code

--- Main.java
+++ Main.java
@@ -6,33 +6,43 @@ class Main
   {
     Scanner scan = new Scanner(System.in);
 
-    System.out.println( "What is you balance (must be greater than 10 and only whole numbers)");
+    System.out.println( "What is you balance (must be greater than 7 and only whole numbers)");
+    //will save users input
     int balance = scan.nextInt();
 
     System.out.println( " Price per gallon of gas (must be less than 5 and only whole numbers)");
+    //will save users input
     int priceGas = scan.nextInt();
 
+   //change your gallons to just gallons 
     int gallons = yourGallons(balance, priceGas) ;
     System.out.println( " You can afford " + gallons + " gallons of gas");
     
-    System.out.println(gas(int gallons));
+    gas(gallons);
+    
   
   }
   static int yourGallons(int balance, int priceGas) 
   {
+  // will divide balance by price of gas
    return balance/priceGas;
   }
-
-  public static void gas(int gallons)
+public static void gas(int gallons  )
   {
-    if(gallons>balance)
+    //will stop when gallons equal 0
+    if(gallons==0)
     {
       System.out.println("Your car is full");
-    }else
+    }
+    //if gallons does not equal 0 this will print
+    else
     {
       System.out.println("You added another gallon");
-      balance--;
+      //subtracts 1 from gallons
+      gallons--;
       gas(gallons);
+  
     }
   }
+  
 }
